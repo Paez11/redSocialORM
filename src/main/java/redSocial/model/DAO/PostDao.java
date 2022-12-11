826b992c4426e1803e5bc2081f04f37605ecc719
miguelllcas <miguelcasly@gmail.com>
2022-12-11 18:43:02 +0100

    second commit

--- src/main/java/redSocial/model/DAO/PostDao.java
+++ src/main/java/redSocial/model/DAO/PostDao.java
@@ -13,7 +13,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class PostDao extends Post{
+	private static Connection con = null;
+	private static EntityManager manager;
+	private static EntityManagerFactory emf=Persistence.createEntityManagerFactory("MySQL");
+
 
     private final static String INSERT = "INSERT INTO post (id,id_user,fecha_creacion,texto) VALUES (NULL,?,?,?)";
     private final static String UPDATE = "UPDATE post SET fecha_modificacion=?,texto=? WHERE id=?";
@@ -39,235 +47,78 @@ public class PostDao extends Post{
         super();
     }
     public PostDao(int id){
-        this.getById(id);
+        //this.getById(id);
     }
 
     public void save() {
-        //INSERT
-        Connection cn = Connect.getConnect();
-        if(cn != null) {
-            PreparedStatement ps;
+    	manager = emf.createEntityManager();
+        manager.getTransaction().begin();
             try {
-                ps = cn.prepareStatement(INSERT,Statement.RETURN_GENERATED_KEYS);
-                ps.setInt(1, this.getUserName().getId());
-                ps.setDate(2, Date.valueOf(LocalDate.now()));
-                ps.setString(3, this.getText());
-                ps.executeUpdate();  //devuelve 1 si ha salido bien
-                ResultSet rs = ps.getGeneratedKeys();
-                if(rs.next()) {
-                    this.setId(rs.getInt(1));
-                }
-                ps.close();
-                rs.close();
-            } catch (SQLException e) {
-                Log.severe("Error al insertar el post: " + e.getMessage());
+                manager.createQuery(INSERT)
+                        .setParameter(1, this.getUserName().getId())
+                        .setParameter(2, Date.valueOf(LocalDate.now()))
+                        .setParameter(3, this.getText());
+                manager.getTransaction().commit();
+                manager.close();
+            } catch (Exception e) {
+                Log.severe("Error al insertar usuario: " + e.getMessage());
             }
-        }
-}
+	}
 
     public void delete() {
-        //UPDATE
-        Connection cn = Connect.getConnect();
-        if(cn != null) {
-            PreparedStatement ps;
+    	manager = emf.createEntityManager();
+        manager.getTransaction().begin();
             try {
-                ps = cn.prepareStatement(DELETE);
-                ps.setInt(1, this.getId());
-                if(ps.executeUpdate()==1) {
-                    this.setId(-1);
-                }
-                ps.close();
-            } catch (SQLException e) {
-                Log.severe("Error al borrar el post: "+e.getMessage());
+                manager.createQuery(DELETE)
+                        .setParameter(1, this.getId());
+                manager.getTransaction().commit();
+                manager.close();
+            } catch (Exception e) {
+            	
             }
-
-        }
+                
     }
 
     public void update() {
-        if(this.getId()!=-1) {
-            //UPDATE
-            Connection cn = Connect.getConnect();
-            if(cn != null) {
-                PreparedStatement ps;
-                try {
-                    ps = cn.prepareStatement(UPDATE);
-                    ps.setDate(1, Date.valueOf(LocalDate.now()));
-                    ps.setString(2, this.getText());
-                    ps.setInt(3, this.getId());
-                    ps.executeUpdate();  //devuelve 1 si funciona
-                    ps.close();
-                } catch (SQLException e) {
-                    Log.severe("Error al actualizar el post: "+e.getMessage());
-                }
-
+        manager = emf.createEntityManager();
+        manager.getTransaction().begin();
+            try {
+                manager.createQuery(UPDATE)
+                        .setParameter(1,Date.valueOf(LocalDate.now()))
+                        .setParameter(2, this.getText())
+                        .setParameter(3, this.getId());                     
+                manager.getTransaction().commit();
+                manager.close();
+            } catch (Exception e) {
+                Log.severe("Error al actualizar usuario: " + e.getMessage());
             }
-        }
     }
 
-    public static List<PostDao> getAll(){
-        List<PostDao> result = new ArrayList();
-        Connection cn = Connect.getConnect();
-        UserDao user = new UserDao();
-        if(cn != null) {
-            PreparedStatement ps;
-            try {
-                ps = cn.prepareStatement(SELECTALL);
-                if(ps.execute()) {
-                    ResultSet rs = ps.getResultSet();
-                    while(rs.next()) {
-                        PostDao p = new PostDao(user.getById(rs.getInt("id_user")),
-                                rs.getInt("id"),
-                                rs.getDate("fecha_creacion"),
-                                rs.getDate("fecha_modificacion"),
-                                rs.getString("texto"));
-                        result.add(p);
-                    }
-                    rs.close();
-                }
-                ps.close();
-            } catch (SQLException e) {
-                Log.severe("Error al obtener todos los posts"+e.getMessage());
-            }
-        }
-        return result;
+   /* public static List<PostDao> getAll(){
+     
     }
 
     public Post getById(int id) {
-        Connection cn = Connect.getConnect();
-        UserDao user = new UserDao();
-        PostDao post = new PostDao(userName, id, dateCreate, dateUpdate, text, comments, likes);
-        CommentDao comments = null;
-        if(cn != null) {
-            PreparedStatement ps;
-            try {
-                ps = cn.prepareStatement(SELECTBYID);
-                ps.setInt(1, id);
-                if(ps.execute()) {
-                    ResultSet rs = ps.getResultSet();
-                    if(rs.next()) {
-                        id=rs.getInt(1);
-                        userName = user.getById(rs.getInt("id_user"));
-                        post.dateCreate = rs.getDate("fecha_creacion");
-                        post.dateUpdate = rs.getDate("fecha_modificacion");
-                        post.text = rs.getString("texto");
-                    }
-                }
-                ps.close();
-            } catch (SQLException e) {
-                Log.severe("Error al obtener el post con id: "+id+"\n"+e.getMessage());
-            }
-
-        }
-        return post;
+     
     }
 
     public List<Comment> getComments(){
-        List<Comment> comments = new ArrayList();
-        Connection cn = Connect.getConnect();
-        if (cn != null) {
-            PreparedStatement ps = null;
-            try {
-                ps = cn.prepareStatement(SELECTCOMMENTS);
-                ps.setInt(1, this.id);
-                if (ps.execute()) {
-                    ResultSet rs = ps.getResultSet();
-                    while (rs.next()) {
-
-                    }
-                    rs.close();
-                }
-                ps.close();
-            } catch (SQLException e) {
-                Log.severe("Error al obtener los comentarios del post con id: "+id+"\n"+e.getMessage());
-            }
-        }
-        return comments;
+       
     }
 
     public static List<PostDao> getAllByUser(int id){
-        UserDao user = new UserDao();
-        Connection cn = Connect.getConnect();
-        List<PostDao> posts = new ArrayList<PostDao>();
-
-        if(cn != null) {
-            try {
-                PreparedStatement ps = cn.prepareStatement(SELECTBYUSER);
-                ps.setInt(1, id);
-                ResultSet rs = ps.executeQuery();
-                while(rs.next()) {
-                    PostDao p = new PostDao();
-                    p.setId(rs.getInt("id"));
-                    p.setText(rs.getString("texto"));
-                    p.setDateCreate(rs.getDate("fecha_creacion"));
-                    p.setUserName(user.getById(rs.getInt("id_user")));
-                    posts.add(p);
-                }
-                rs.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return posts;
+        
     }
 
     public void saveLike(User u, Post p) {
-        Connection con = Connect.getConnect();
-        con = Connect.getConnect();
-        if (con != null){
-            PreparedStatement st = null;
-            try {
-                st = con.prepareStatement(INSERTLIKE);
-                st.setInt(1, u.getId());
-                st.setInt(2,p.getId());
-                st.executeUpdate();
-                st.close();
-            } catch (SQLException e) {
-                Log.severe("Error al guardar el like: " + e.getMessage());
-            }
-        }
+     
     }
 
     public void deleteLike(User u) {
-        Connection con = Connect.getConnect();
-        con = Connect.getConnect();
-        if (con != null){
-            PreparedStatement st = null;
-            try {
-                st = con.prepareStatement(DELETELIKE);
-                st.setInt(1,u.getId());
-                st.executeUpdate();
-                st.close();
-            } catch (SQLException e) {
-                Log.severe("Error al borrar el like: " + e.getMessage());
-            }
-        }
+       
     }
 
     public Set<User> getAllLikes(Post p){
-        Connection con = Connect.getConnect();
-        con = Connect.getConnect();
-        Set<User> likeds = new HashSet<>();
-        if (con != null){
-            PreparedStatement st = null;
-            try {
-                st = con.prepareStatement(SELECTALLLIKES);
-                st.setInt(1,p.getId());
-                if (st.execute()){
-                    ResultSet rs = st.getResultSet();
-                    while (rs.next()){
-                        UserDao liked = new UserDao();
-                        liked.setId(rs.getInt(1));
-                        UserDao add= (UserDao) liked.getById(liked.getId());
-                        likeds.add(add);
-                    }
-                    rs.close();
-                }
-                st.close();
-            } catch (SQLException e) {
-                Log.severe("Error al obtener los likes: " + e.getMessage());
-            }
-        }
-        return likeds;
-    }
+    	
+    }*/
 }
